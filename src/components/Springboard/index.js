ad19d97ee04d873ec5c4bb6af2538ee7a3582525
Nikhil Benesch <nikhil.benesch@gmail.com>
2016-04-14 17:55:06 -0400

    Add homepage

--- src/components/Springboard/index.js
+++ src/components/Springboard/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+import { styles } from './styles.scss';
 
 export class Springboard extends Component {
   render() {
     return (
-      <section>
+      <section className={styles}>
         <div className="container">
           <div className="row">
-            <h1>Who are you?</h1>
+            <Link to="/incoming" className="col-sm-6 col-sm-offset-3 button">
+              <h3>Incoming residents</h3>
+              <p>Decide where to live.</p>
+            </Link>
+          </div>
+
+          <div className="row">
+            <Link to="/nerds" className="col-sm-6 col-sm-offset-3 button">
+              <h3>Transit planners and existing residents</h3>
+              <p>See where Boston transit is weak.</p>
+            </Link>
           </div>
         </div>
       </section>
